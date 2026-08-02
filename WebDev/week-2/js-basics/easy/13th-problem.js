cfcb16f8968b732dccb38b595fa9f1e5007b3957
Daksh Sharma <dakshsharma784@gmail.com>
2026-08-02 15:51:12 +0530

    restarting from week 2

--- WebDev/week-2/js-basics/easy/13th-problem.js
+++
@@ -1,9 +0,0 @@
-function count(input){
-  let output = 0;
-  for(i in input){
-    output++;
-  }
-  return output;
-}
-
-console.log(count({ a: 1, b: 2, c: 3 }));
--- WebDev/week-2/js-basics/easy/18th-problem.js
+++
@@ -1,9 +0,0 @@
-function addIndex(input){
-  let output = {};
-  for(i in input){
-    output[input[i]["id"]] = input[i];
-  }
-  return output;
-}
-
-console.log(addIndex([{ id: 1, name: "A" }, { id: 2, name: "B" }]))
